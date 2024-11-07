e771cd279a9d5e5574d86e444d596913ece06362
imacapella <gurkankaradas42@gmail.com>
2024-11-07 23:50:14 +0300

    Initial Commit

---
+++ PennyTrackerOnboard/PennyTrackerOnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyTrackerOnboardApp.swift
+//  PennyTrackerOnboard
+//
+//  Created by Gürkan Karadaş on 7.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PennyTrackerOnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
